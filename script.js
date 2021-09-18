24212f289cd77386ca9ff46068f41fa24a08e263
magendiran1 <48393612+magendiran1@users.noreply.github.com>
2021-09-18 21:27:52 +0530

    Add files via upload
    callback hell

---
+++ script.js
@@ -0,0 +1,52 @@
+let count = 10;
+
+
+
+setTimeout(() => {
+
+
+    const heading = document.querySelector("h1");
+    heading.innerText = count--
+
+        setTimeout(() => {
+            const heading = document.querySelector("h1");
+            heading.innerText = count--
+                setTimeout(() => {
+                    const heading = document.querySelector("h1");
+                    heading.innerText = count--
+                        setTimeout(() => {
+                            const heading = document.querySelector("h1");
+                            heading.innerText = count--
+                                setTimeout(() => {
+                                    const heading = document.querySelector("h1");
+                                    heading.innerText = count--
+                                        setTimeout(() => {
+                                            const heading = document.querySelector("h1");
+                                            heading.innerText = count--
+                                                setTimeout(() => {
+                                                    const heading = document.querySelector("h1");
+                                                    heading.innerText = count--
+                                                        setTimeout(() => {
+                                                            const heading = document.querySelector("h1");
+                                                            heading.innerText = count--
+                                                                setTimeout(() => {
+                                                                    const heading = document.querySelector("h1");
+                                                                    heading.innerText = count--
+                                                                        setTimeout(() => {
+                                                                            const heading = document.querySelector("h1");
+                                                                            heading.innerText = count--
+                                                                                setTimeout(() => {
+                                                                                    const heading = document.querySelector("h1");
+                                                                                    heading.innerText = "Happy Independence Day "
+
+                                                                                }, 1000)
+                                                                        }, 1000)
+                                                                }, 1000)
+                                                        }, 1000)
+                                                }, 1000)
+                                        }, 1000)
+                                }, 1000)
+                        }, 1000)
+                }, 1000)
+        }, 1000)
+}, 1000)
